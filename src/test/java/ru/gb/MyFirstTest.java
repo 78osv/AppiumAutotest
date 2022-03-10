f09bccfe9006bb3e93806ff08d7b9aae4eb11e4d
Vakulina <Vakulina@sevastopol.softlab.ru>
2022-03-10 08:50:41 +0300

    Homework №1 done

---
+++ src/test/java/ru/gb/MyFirstTest.java
@@ -0,0 +1,93 @@
+package ru.gb;
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+
+public class MyFirstTest {
+
+    @Test
+    public void CheckEmptyEmail() throws Exception {
+
+        // Устанавливаем capabilities.
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "/Users/vakulina/Downloads/Android-NativeDemoApp-0.2.1.apk");
+
+//      Устанавливаем и открываем приложение.
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(2000);
+//      Нажимаем Login в меню
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+//      Нажимаем Login на странице логин/пароль.
+        MobileElement loginButton = (MobileElement) driver.findElementByAccessibilityId("button-LOGIN");
+        loginButton.click();
+        Thread.sleep(2000);
+//      Проверяем текст ошибки.
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
+        Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
+    }
+
+    @Test
+    public void CheckEmptyPassword() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "/Users/vakulina/Downloads/Android-NativeDemoApp-0.2.1.apk");
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(2000);
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+        MobileElement inputEmail = (MobileElement) driver.findElementByAccessibilityId("input-email");
+        inputEmail.sendKeys("abc@mail.ru");
+        MobileElement loginButton = (MobileElement) driver.findElementByAccessibilityId("button-LOGIN");
+        loginButton.click();
+        Thread.sleep(2000);
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView");
+        Assert.assertEquals(errorText.getText(), "Please enter at least 8 characters");
+    }
+
+    @Test
+    public void CheckInvalidEmail() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "/Users/vakulina/Downloads/Android-NativeDemoApp-0.2.1.apk");
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Thread.sleep(2000);
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+        MobileElement inputEmail = (MobileElement) driver.findElementByAccessibilityId("input-email");
+        inputEmail.sendKeys("abcmail.ru");
+        MobileElement inputPassword = (MobileElement) driver.findElementByAccessibilityId("input-password");
+        inputPassword.sendKeys("12345678");
+        MobileElement loginButton = (MobileElement) driver.findElementByAccessibilityId("button-LOGIN");
+        loginButton.click();
+        Thread.sleep(2000);
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView");
+        Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
+    }
+
+}
